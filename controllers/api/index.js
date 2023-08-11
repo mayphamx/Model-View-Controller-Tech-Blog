8b0c632f1de52d1ca3dc195335de3977bd6dce0d
May <maypham@utexas.edu>
2023-08-11 01:08:43 -0500

    added comment code and handlebars/routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const blogRoutes = require('./blogRoutes');
+const commentRoutes = require('./commentRoutes');
 
+router.use('/comments', commentRoutes);
 router.use('/users', userRoutes);
 router.use('/blogs', blogRoutes);
 
